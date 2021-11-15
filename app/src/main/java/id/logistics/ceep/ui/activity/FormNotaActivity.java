3d6312ae28caeb58326dd72581bf192e3aee2058
Junior Fernandes <afernandes@id-logistics.com.br>
2021-11-15 17:50:34 -0300

    Recycler View pt 1 - 4 (Adicionando notas na lista) | Adicionando o menu no formulário

--- app/src/main/java/id/logistics/ceep/ui/activity/FormNotaActivity.java
+++ app/src/main/java/id/logistics/ceep/ui/activity/FormNotaActivity.java
@@ -3,6 +3,7 @@ package id.logistics.ceep.ui.activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
 
 import id.logistics.ceep.R;
 
@@ -12,6 +13,12 @@ public class FormNotaActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_form_nota);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_form_nota_salva, menu);
+        return super.onCreateOptionsMenu(menu);
     }
 
 }
